26291f1035f1016b4845a1b73951f756a3e8429d
redanaser0@gmail.com <redanaser0@gmail.com>
2021-08-18 18:13:01 +0200

    اخر تحديث

--- lib/SCR/all_customer_tap2.dart
+++ lib/SCR/all_customer_tap2.dart
@@ -1,19 +1,314 @@
+import 'package:anjum/controllers/allChequesController.dart';
 import 'package:anjum/controllers/all_routes.dart';
+import 'package:anjum/network/json/get_employee_data_json.dart';
+import 'package:dotted_line/dotted_line.dart';
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
+import 'package:google_maps_flutter/google_maps_flutter.dart';
+
+import 'myMapScr.dart';
 
 class All_customer_tap2 extends StatefulWidget {
-  const All_customer_tap2({Key  key}) : super(key: key);
+  const All_customer_tap2({Key key}) : super(key: key);
 
   @override
   _All_customer_tap2State createState() => _All_customer_tap2State();
 }
 
 class _All_customer_tap2State extends State<All_customer_tap2> {
+
+
   @override
   Widget build(BuildContext context) {
-    return Scaffold(body: GetBuilder<All_routesController>(builder: (logic){
-      // logic.allRoutes[]
+    Size _size=MediaQuery.of(context).size;
+    return Scaffold(body: GetBuilder<All_routesController>(builder: (logic) {
+      return ListView.builder(itemCount: logic
+          .allCustomarInRouts()
+          .length, itemBuilder: (context, pos) {
+
+return item(data:logic
+    .allCustomarInRouts()[pos]  ,size: _size);
+      });
     },),);
   }
+  bool iscust(AllCustomers data) {
+    if (Get.find<AllChequesController>().customer != null) {
+      return Get.find<AllChequesController>().customer.customerInfo.id ==
+          data.customerInfo.id;
+    }else{
+      return false;
+    }
+  }
+  Widget item({size, AllCustomers data}) {
+
+
+    return Padding(
+      padding: const EdgeInsets.all(8.0),
+      child: Container(
+        decoration: BoxDecoration(
+          color: Colors.white,
+          borderRadius: BorderRadius.circular(20),
+          // boxShadow: [
+          //   BoxShadow(
+          //     color: Colors.black26,
+          //     blurRadius: 10.0,
+          //     offset: const Offset(0.0, 10.0),
+          //   ),
+          // ],
+        ),
+        height: size.height * .22,
+        child: Row(
+          mainAxisAlignment: MainAxisAlignment.start,
+          children: [
+            ClipRRect(
+              borderRadius: BorderRadius.circular(8.0),
+              child: Container(
+                padding: EdgeInsets.all(16),
+                child: Image.network(
+                  data.customerInfo.image,
+                  width: size.height * .08,
+                  height: size.height * .08,
+                  fit: BoxFit.fill,
+                ),
+              ),
+            ),
+            Container(width: 1, height: size.height * .2, color: Colors.cyan),
+            SizedBox(
+              width: 8,
+            ),
+            Expanded(
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  SizedBox(
+                    height: 8,
+                  ),
+                  Text(
+                    data.customerInfo.customerNameEn,
+                    maxLines: 2,
+                    overflow: TextOverflow.ellipsis,
+                    textAlign: TextAlign.left,
+                    style: TextStyle(fontSize: 18, fontWeight: FontWeight.bold),
+                  ),
+                  SizedBox(
+                    height: 4,
+                  ),
+                  Container(
+                    width: size.width * .5,
+                    child: Row(
+                      mainAxisAlignment: MainAxisAlignment.start,
+                      mainAxisSize: MainAxisSize.max,
+                      //+"\n"+data.customerInfo.area2??
+                      children: [
+                        Icon(Icons.add_location),
+                        Text(data.customerInfo.phoneNo??""),
+                        Expanded(child: Container()),
+                        //if(iscust(data))     Image.asset('assets/images/fast.png')
+                      ],
+                    ),
+                  ),
+                  Expanded(child: Container()),
+                  Container(
+                    width: size.width * .7,
+                    height: 10,
+                    child: DottedLine(
+                      direction: Axis.horizontal,
+                      lineLength: double.infinity,
+                      lineThickness: 1.0,
+                      dashLength: 4.0,
+                      dashColor: Colors.cyan,
+                      dashRadius: 0.0,
+                      dashGapLength: 4.0,
+                      dashGapColor: Colors.transparent,
+                      dashGapRadius: 0.0,
+                    ),
+                  ),
+                  Padding(
+                    padding: const EdgeInsets.only(bottom: 8),
+                    child: Row(
+                      children: [
+                        Icon(Icons.info_sharp),
+                        InkWell(
+                            onTap: () {
+                              Get.bottomSheet(
+                                Stack(
+                                  children: <Widget>[
+                                    //...bottom card part,
+                                    Container(
+                                      width: MediaQuery.of(context).size.width,
+                                      padding: EdgeInsets.only(
+                                        top: Consts.avatarRadius + Consts.padding,
+                                        bottom: Consts.padding,
+                                        left: Consts.padding,
+                                        right: Consts.padding,
+                                      ),
+                                      margin: EdgeInsets.only(
+                                          top: Consts.avatarRadius),
+                                      decoration: new BoxDecoration(
+                                        color: Colors.white,
+                                        shape: BoxShape.rectangle,
+                                        borderRadius:
+                                        BorderRadius.circular(Consts.padding),
+                                        // boxShadow: [
+                                        //   BoxShadow(
+                                        //     color: Colors.black26,
+                                        //     blurRadius: 10.0,
+                                        //     offset: const Offset(0.0, 10.0),
+                                        //   ),
+                                        // ],
+                                      ),
+                                      child: SingleChildScrollView(
+                                        child: Column(
+                                          mainAxisAlignment:
+                                          MainAxisAlignment.spaceAround,
+                                          crossAxisAlignment:
+                                          CrossAxisAlignment.start,
+                                          //  mainAxisSize: MainAxisSize.min,
+                                          // To make the card compact
+                                          children: <Widget>[
+                                            Text(
+                                              'customername'.tr+' : ' +'${data.customerInfo.customerNameEn}',
+                                              style: TextStyle(
+                                                fontWeight: FontWeight.bold,
+                                              ),
+                                            ),
+                                            Text(
+                                              'Email'+' : '+'${data.customerInfo.email}',
+                                              style: TextStyle(
+                                                fontWeight: FontWeight.bold,
+                                              ),
+                                            ),
+                                            Text(
+                                              'customertype'.tr+' :  '+'${data.customerInfo.customerTypeId}',
+                                              style: TextStyle(
+                                                fontWeight: FontWeight.bold,
+                                              ),
+                                            ),
+                                            Text(
+                                              'phone'.tr+' : '+'${data.customerInfo.phoneNo}',
+                                              style: TextStyle(
+                                                fontWeight: FontWeight.bold,
+                                              ),
+                                            ),
+                                            Text(
+                                              'Fax : ${data.customerInfo.fax}',
+                                              style: TextStyle(
+                                                fontWeight: FontWeight.bold,
+                                              ),
+                                            ),
+                                            Text(
+                                              'state'.tr+' : '+'${data.customerInfo.status}',
+                                              style: TextStyle(
+                                                fontWeight: FontWeight.bold,
+                                              ),
+                                            ),
+                                            Text(
+                                              'city'.tr+' : '+'${data.customerInfo.cityId}',
+                                              style: TextStyle(
+                                                fontWeight: FontWeight.bold,
+                                              ),
+                                            ),
+                                            Text(
+                                              'creditlimit'.tr+' : '+'${data.customerInfo.creditLimit}',
+                                              style: TextStyle(
+                                                fontWeight: FontWeight.bold,
+                                              ),
+                                            ),
+                                            Text(
+                                              'Cheque Due Date : ${data.customerInfo.chequeDueDate}',
+                                              style: TextStyle(
+                                                fontWeight: FontWeight.bold,
+                                              ),
+                                            ),
+                                            Text(
+                                              'paymenttype'.tr+' : '+'${data.customerInfo.paymentType}',
+                                              style: TextStyle(
+                                                fontWeight: FontWeight.bold,
+                                              ),
+                                            ),
+                                            Text(
+                                              'location'.tr+' : '+'${data.customerInfo.area1}',
+                                              style: TextStyle(
+                                                fontWeight: FontWeight.bold,
+                                              ),
+                                            ),
+                                            Text(
+                                              'pricelist'.tr+' : '+'${data.priceListsInfo[0].id}',
+                                              style: TextStyle(
+                                                fontWeight: FontWeight.bold,
+                                              ),
+                                            )
+                                          ],
+                                        ),
+                                      ),
+                                    ),
+
+                                    Positioned(
+                                      left: Consts.padding,
+                                      right: Consts.padding,
+                                      child: CircleAvatar(
+                                        //backgroundColor: Colors.blueAccent,
+                                        // backgroundImage: NetworkImage(
+                                        //     data.customerInfo.image,
+                                        //     scale: .2),
+                                        radius: Consts.avatarRadius,
+                                        child: ClipOval(
+                                          child: Image.network(
+                                            data.customerInfo.image,
+                                            fit: BoxFit.fill,
+                                          ),
+                                        ),
+                                      ),
+                                    ),
+                                    //...top circlular image part,
+                                  ],
+                                ),
+                                // backgroundColor: Colors.white54,
+                                // isDismissible: true,
+                                // shape: RoundedRectangleBorder(
+                                //     borderRadius: BorderRadius.circular(10),
+                                //     side: BorderSide(
+                                //         color: Colors.white54,
+                                //         style: BorderStyle.solid,
+                                //         width: 2)),
+                              );
+                            },
+                            child: Text('Customer info')),
+                        SizedBox(
+                          width: 16,
+                        ),
+                        Icon(Icons.map),
+                        InkWell(
+                            onTap: () {
+                              //   location
+                              List<String> l =
+                              data.customerInfo.location.split(',');
+                              // var lat=double.tryParse(l[0].trim());
+                              // var Lng=double.tryParse(l[1].trim());
+
+                              LatLng loc = LatLng(double.tryParse(l[0].trim()),
+                                  double.tryParse(l[1].trim()));
+                              Get.to(()=>MyMapScr(
+                                loc: loc,
+                                name: data.customerInfo.customerNameEn,
+                              ));
+                            },
+                            child: Text('view map')),
+                      ],
+                    ),
+                  ),
+                ],
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
+class Consts {
+  Consts._();
+
+  static const double padding = 16.0;
+  static const double avatarRadius = 70;
 }
--- lib/controllers/all_routes.dart
+++ lib/controllers/all_routes.dart
@@ -1,38 +1,75 @@
 import 'package:anjum/network/json/get_employee_data_json.dart';
 import 'package:get/get.dart';
+import 'package:intl/intl.dart';
 
 import 'allCustomersControllers.dart';
 
 class All_routesController extends GetxController {
-
   List<AllRoutes> allRoutes;
-  void updateAllRoutes(List<AllRoutes> allroutes){
-    this.allRoutes=allroutes;
+Map<String,AllCustomers>mapOfCustomar={};
 
+  void updateAllRoutes(List<AllRoutes> allroutes) {
+    this.allRoutes = allroutes;
 
     update();
   }
 
-  List<AllCustomers> allCustomarInRouts(){
-    List<AllCustomers>listCustomers=[];
+  @override
+  void onInit() {
+    super.onInit();
+    Get.find<AllCustomersControllers>().allCustomers.forEach((element) {
 
-    List<ListRoutesInfo>listRoutes=[];
+      mapOfCustomar[element.customerInfo.id]=element;
+
+    });
+  }
+
+  List<AllCustomers> allCustomarInRouts() {
+    DateTime time = DateTime.now();
+    List<AllCustomers>allCustomers=[];
+
+
+    List<ListRoutesInfo> listRoutes = [];
     // for(int i=0;i<Get.find<AllCustomersControllers>().allCustomers.length;i++){
     //   for(int p=0;p<allRoutes.length){}
     // }
 
-    for(int p=0;p<allRoutes.length;p++){
-      for(int i=0;i<allRoutes[i].listRoutesInfo.length;i++){
-
+    for (int p = 0; p < allRoutes.length; p++) {
+      for (int i = 0; i < allRoutes[i].listRoutesInfo.length; i++) {
         listRoutes.add(allRoutes[p].listRoutesInfo[i]);
       }
-
     }
-    listCustomers.forEach((element) {
+    //  listCustomers.forEach((element) {
+    //    final DateFormat formatter = DateFormat('yyyy-MM-dd');
+    //    DateTime from=DateTime.parse(element.);
+    // if(time.isBefore(element.))
+    //  });
+    final DateFormat formatter = DateFormat('EEEE');
+    listRoutes.forEach((element) {
+      print('wednesday    ${element.wednesday}' );
+      print('customerId    ${element.customerId}' );
+      print('element    ${formatter.format(time).toLowerCase()}' );
 
+      Map<String, String> mapOfDay = {};
+      mapOfDay["sunday"] = element.sunday;
+      mapOfDay["monday"] = element.monday;
+      mapOfDay["tuesday"] = element.tuesday;
+      mapOfDay["wednesday"] = element.wednesday;
+      mapOfDay["thursday"] = element.thursday;
+      mapOfDay["saturday"] = element.saturday;
+      mapOfDay["friday"] = element.friday;
+
+      DateTime from = DateTime.parse(element.startDate);
+      DateTime todate = DateTime.parse(element.endDate);
+      if (time.isAfter(from) && time.isBefore(todate)) {
+        if (element.schedule.toLowerCase() == "weekly") {
+
+          if (mapOfDay[formatter.format(time).toLowerCase()] == "yes") {
+          print(mapOfCustomar);
+             allCustomers.add(mapOfCustomar[element.customerId]);
+          }
+        }
+      }
     });
-
-
-  }
-
+ return allCustomers; }
 }
